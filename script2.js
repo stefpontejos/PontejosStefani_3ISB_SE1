c71771f548c5a5628a33972e93ed6cf39bc18692
stefpontejos <stefanimari.pontejos.iics@ust.edu.ph>
2021-09-14 12:18:37 +0800

    added script2.js

---
+++ script2.js
@@ -0,0 +1,10 @@
+function isTeenager(){
+    let age = 14;
+
+    if(age >= 13 || age <=19) {
+        console.log("you are a teenager");
+    }
+    else {
+        console.log("you are not a teenager");
+    }
+}
