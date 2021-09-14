295366a3eab2e9f50e4927f57f7271f48fdbbf75
stefpontejos <stefanimari.pontejos.iics@ust.edu.ph>
2021-09-14 12:32:52 +0800

    added script4.js

---
+++ script4.js
@@ -0,0 +1,12 @@
+class Rectangle{
+    constructor(length,height){
+        this.length=length;
+        this.height=height;
+    }
+    get computeArea(){
+        return this.computerArea();
+    }
+    computeArea(){
+        return this.length * this.height;
+    }
+}
