f5c3b0addfd1369f949303b1b8fa293958bb0fea
stefpontejos <stefanimari.pontejos.iics@ust.edu.ph>
2021-09-14 12:57:01 +0800

    added script8

---
+++ script6.js
@@ -0,0 +1,16 @@
+class Dog{
+    constructor(name, breed, color){
+        this.name=name;
+        this.breed=breed;
+        this.color=color;
+    }
+    displayInfo(){
+        console.log(Dog.name + Dog.breed + Dog.color);
+    }
+}
+
+Dog.prototype.eat(food)= fucntion(){
+    if(Dog.name == bantay || DOMQuad.food==fish){
+        console.log("Bantay likes fish");
+    }
+}
--- script8.js
+++ script8.js
@@ -1,1 +1,17 @@
-school
+class School{
+    constructor(name, location, population){
+        this.name=name;
+        this.location = location;
+        this.population = population;
+    }
+display Info(){
+    console.log(School.name + School.location + School.population)
+}
+School.prototype.offer(courses)=function(){
+    if(School.name == UST || School.courses == architecture){
+        console.log("UST offers architecture, nursing, information systems")
+    }
+}
+let mySchool = bind()
+}
+}
