8d5f2070dd3ca887f92f9f52faf06861942da0ed
stefpontejos <stefanimari.pontejos.iics@ust.edu.ph>
2021-09-14 12:15:22 +0800

    added script1.js

---
+++ script1.js
@@ -0,0 +1,8 @@
+let myself=(
+    firstname = Stefani,
+    lastname = Pontejos,
+    age = 20,
+    subject = "ICS 2609",
+    introduce: function(){"hello, I am Stefani Pontejos"},
+    enrolled: function(){"I am enrolled in ICS 2609"}
+)
---
+++ script7.js
@@ -0,0 +1,9 @@
+const numbers =[4, 6, 2, 9, 5];
+const sum = numbers.reduce(
+    (accumulator, currentValue) => accumulator + currentValue);
+
+let sum = 0;
+for (let n of numbers)
+    sum + n;
+
+    console.log(sum);
